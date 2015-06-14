4ef15e162773fab8cae2a22eb0b05b7c73022f92
Stephen R. Veit <srveit@gmail.com>
2015-06-14 18:38:09 -0500

    add setContents and createChunk

--- riff/riff.js
+++ riff/riff.js
@@ -1,15 +1,71 @@
 (function (module) {
+  var _ = require('lodash'),
+      chunkClasses = {};
   /**
    * The basic data structure in a RIFF file.
    * @constructor
-   * @param {string} type - The four character type of chunk.
-   * @param {string} size - the number of bytes in the chunk not
+   * @param {string} type - four character type of chunk.
+   * @param {string} size - number of bytes in the chunk not
    *   including the chunkType.
    */
-  function BaseChunk() {
-    this.type = '    ';
-    this.size = 0;
+  function BaseChunk(type, size) {
+    if (_.isUndefined(type)) {
+      type = '    ';
+    }
+    if (_.isUndefined(size)) {
+      size = 0;
+    }
+    var contents = new Buffer(8);
+    contents.write(type, 0, 4, 'ascii');
+    contents.writeUInt32BE(size, 4);
+    this.setContents(contents);
   }
+
+  /**
+   * Sets ths encoded contents of a RIFF file.
+   * @name setContents
+   * @param {Buffer} contents - encoded byte contents of the RIFF file
+   */
+  BaseChunk.prototype.setContents = function (contents) {
+    this.contents = contents;
+    this.bufferLength = this.contents.length;
+    this.type = this.contents.toString('ascii', 0, 4);
+    this.size = this.contents.readUInt32BE(4);
+  };
+
+  /**
+   * Returns the class to be used for decoding a chunk
+   * @name chunkClass
+   * @param {string} chunkType - 4-character RIFF chunk type
+   * @returns {function} - constructor for decoding the RIFF chunk
+   */
+  BaseChunk.chunkClass = function (chunkType) {
+    return chunkClasses[chunkType] || BaseChunk;
+  };
+
+  /**
+   * Creates a chunk of the appropriate class from the contents of a RIFF file
+   * @name createChunk
+   * @param {Buffer} contents - encoded byte contents of the RIFF file
+   * @returns {object} - chunk of the appropriate class
+   */
+  BaseChunk.createChunk = function (contents) {
+    var chunkType = contents.toString('ascii', 0, 4),
+        chunkClass = this.chunkClass(chunkType),
+        chunk = new chunkClass();
+    chunk.setContents(contents);
+    return chunk;
+  };
+
+  /**
+   * Registers the class to be used for decoding a chunk
+   * @name registerChunkClass
+   * @param {string} chunkType - 4-character RIFF chunk type
+   * @returns {function} chunkClass - constructor for decoding the RIFF chunk
+   */
+  BaseChunk.registerChunkClass = function (chunkType, chunkClass) {
+    chunkClasses[chunkType] = chunkClass;
+  };
 
   function Riff() {
   }
--- spec/riff.spec.js
+++ spec/riff.spec.js
@@ -1,8 +1,36 @@
-var riff = require('../riff/riff');
+var riff = require('../riff/riff'),
+      util = require('util'),
+    _ = require('lodash');
 
 describe('riff', function () {
   describe('BaseChunk', function () {
     var chunk;
+    describe('with type and size parameters', function () {
+      beforeEach(function (done) {
+        chunk = new riff.BaseChunk('RIFF', 5);
+        done();
+      });
+      it('should return a chunk', function (done) {
+        expect(chunk).not.toBeUndefined();
+        done();
+      });
+      it('should have a type of "RIFF"', function (done) {
+        expect(chunk.type).toBe('RIFF');
+        done();
+      });
+      it('should have bufferLength', function (done) {
+        expect(chunk.bufferLength).toBe(8);
+        done();
+      });
+      it('should have a size', function (done) {
+        expect(chunk.size).toBe(5);
+        done();
+      });
+      it('should contents', function (done) {
+        expect(Buffer.isBuffer(chunk.contents)).toBe(true);
+        done();
+      });
+    });
     describe('with no parameters', function () {
       beforeEach(function (done) {
         chunk = new riff.BaseChunk();
@@ -16,12 +44,91 @@ describe('riff', function () {
         expect(chunk.type).toBe('    ');
         done();
       });
+      it('should have bufferLength', function (done) {
+        expect(chunk.bufferLength).toBe(8);
+        done();
+      });
       it('should have a size of 0', function (done) {
         expect(chunk.size).toBe(0);
         done();
       });
-      it('should have no content', function (done) {
-        expect(chunk.contents).toBe('');
+      it('should have contents', function (done) {
+        var expectedContents = new Buffer(8);
+        expectedContents.writeUInt32BE(0x20202020, 0);
+        expectedContents.writeUInt32BE(0, 4);
+        expect(chunk.contents.length).toBe(8);
+        _.forEach(chunk.contents, function (byte, i) {
+          expect(byte).toBe(expectedContents[i]);
+        });
+        done();
+      });
+      it('should have a type of "    "', function (done) {
+        expect(chunk.type).toBe('    ');
+        done();
+      });
+      it('should have bufferLength', function (done) {
+        expect(chunk.bufferLength).toBe(8);
+        done();
+      });
+      it('should have a size', function (done) {
+        expect(chunk.size).toBe(0);
+        done();
+      });
+      describe('and setContents called', function () {
+        beforeEach(function (done) {
+          var contents = new Buffer(12);
+          contents.writeUInt32BE(0x52494646, 0);
+          contents.writeUInt32BE(4, 4);
+          chunk.setContents(contents);
+          done();
+        });
+        it('should have a type of "RIFF"', function (done) {
+          expect(chunk.type).toBe('RIFF');
+          done();
+        });
+        it('should have a bufferLength', function (done) {
+          expect(chunk.bufferLength).toBe(12);
+          done();
+        });
+        it('should have a size', function (done) {
+          expect(chunk.size).toBe(4);
+          done();
+        });
+      });
+    });
+    describe('chunkClass', function () {
+      var MyClass = function () {};
+      beforeEach(function (done) {
+        riff.BaseChunk.registerChunkClass('mcla', MyClass);
+        done();
+      });
+      it('should return the chunk class', function (done) {
+        expect(riff.BaseChunk.chunkClass('mcla')).toBe(MyClass);
+        done();
+      });
+    });
+    describe('createChunk with returned chunk', function () {
+      var MyClass, chunk;
+      beforeEach(function (done) {
+        var contents = new Buffer(12);
+        contents.writeUInt32BE(0x52494646, 0);
+        contents.write('mcla', 0, 4, 'ascii');
+        MyClass = function () {};
+        util.inherits(MyClass, riff.BaseChunk);
+        riff.BaseChunk.registerChunkClass('mcla', MyClass);
+        chunk = riff.BaseChunk.createChunk(contents);
+        done();
+      });
+      it('should exist', function (done) {
+        expect(chunk).not.toBeUndefined();
+        done();
+      });
+      it('should have the correct class', function (done) {
+        expect(chunk.constructor).toBe(MyClass);
+        done();
+      });
+      it('should have a type', function (done) {
+        expect(chunk.type).toBe('mcla');
         done();
       });
     });
