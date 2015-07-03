49f46e89cffe7a7e662a284b159f2f992c863628
Stephen R. Veit <srveit@gmail.com>
2015-07-03 17:58:48 -0500

    add RiffForm class

---
+++ bin/readRiffFile.js
@@ -0,0 +1,20 @@
+'use strict';
+var fs = require('fs'),
+  Chunk = require('../riff/chunk'),
+  RiffForm = require('../riff/riffForm');
+
+function readRiffFile() {
+  var filename = process.argv[2],
+      buffer = fs.readFileSync(filename),
+      chunk;
+  console.log(filename, buffer.length);
+  chunk = Chunk.createChunkFromBuffer({contents: buffer});
+  if (chunk) {
+    console.log(chunk.description());
+  } else {
+    console.log('unable to parse', buffer);
+  }
+}
+
+readRiffFile();
+
