1202f78d64473c0e6e6bfcaf2219d7c3774f1c20
Stephen R. Veit <srveit@gmail.com>
2015-07-05 10:44:52 -0500

    use async readFile

--- bin/readRiffFile.js
+++ bin/readRiffFile.js
@@ -3,11 +3,15 @@ var fs = require('fs'),
   riff = require('../riff');
 
 function readRiffFile(filename) {
-  var buffer = fs.readFileSync(filename),
-      chunk;
-  console.log(filename, buffer.length);
-  chunk = riff.createChunkFromBuffer({contents: buffer});
-  console.log(chunk.description());
+  fs.readFile(filename, function (err, buffer) {
+    var chunk;
+    if (err) {
+      throw err;
+    }
+    console.log(filename, buffer.length);
+    chunk = riff.createChunkFromBuffer({contents: buffer});
+    console.log(chunk.description());
+  });
 }
 
 readRiffFile(process.argv[2]);
